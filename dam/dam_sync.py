b9981a97d4773130c7657473931c1e46b035286f
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2020-08-12 15:31:41 +0100

    Config command improvements

--- dam/dam_sync.py
+++ dam/dam_sync.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import click
+import os
+import json
 from dam.colors import *
+from pprint import pprint as pp
 
 # -----------------------------------------------------------------------------
 # Group setup
@@ -16,12 +19,84 @@ def cli():
 # Commands
 # -----------------------------------------------------------------------------
 
+config_dir = os.path.expanduser('~/.dam-sync')
+config_path = os.path.join(config_dir, 'config.json')
+
+def get_config():
+    """
+    Returns current config for your environment
+    """
+    if not os.path.isdir(config_dir):
+        os.makedirs(config_dir)
+
+    return 'lol'
+
+    if not os.path.isfile(config_path):
+        return None
+
+
+
+    print('GETTING CONFIG', path)
+
+
+
+
 @cli.command(name='configure')
-def configure():
+@click.option(
+    '--source',
+    default=None,
+    prompt=True,
+    help='Local library path to backup'
+)
+@click.option(
+    '--destination',
+    default=None,
+    prompt=True,
+    help='Target local path to backup to'
+)
+@click.option(
+    '--s3-bucket',
+    default=None,
+    prompt=True,
+    help='AWS S3 bucket name. Can also contain name/path.'
+)
+@click.option(
+    '--aws-profile',
+    default=None,
+    prompt=True,
+    help='AWS CLI profile to use for authentication'
+)
+def configure(source, destination, s3_bucket, aws_profile):
     """ Configure sync"""
     print(green('\nHello from DAM-Sync!'))
     print(green('-' * 80))
     print()
+
+    print('SOURCE', source)
+    print('DESTINATION', destination)
+    print('S3 BUCKET', s3_bucket)
+    print('AWS PROFILE', aws_profile)
+
+    # check config
+    config = get_config()
+    if config:
+        print(red('Found configuration at "{}" '.format(config_path)))
+        if not click.confirm(red('Do you want to continue and overwrite it?')):
+            print(cyan('Skipping...\n'))
+            return
+
+
+    config = dict(
+        source=source,
+        destination=destination,
+        s3_bucket=s3_bucket,
+        aws_profile=aws_profile,
+    )
+
+
+
+
+
 
 
 @cli.command(name='run')
