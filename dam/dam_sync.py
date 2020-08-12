3d736b152db41697f6ba21fe90e1dcb7896eb8f0
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2020-08-12 14:20:01 +0100

    Command line flags to select what to sync

--- dam/dam_sync.py
+++ dam/dam_sync.py
@@ -16,9 +16,59 @@ def cli():
 # Commands
 # -----------------------------------------------------------------------------
 
+@cli.command(name='configure')
+def configure():
+    """ Configure sync"""
+    print(green('\nHello from DAM-Sync!'))
+    print(green('-' * 80))
+    print()
+
+
 @cli.command(name='run')
-def run():
+@click.option('--disk/--skip-disk', default=False)
+@click.option('--cloud/--skip-cloud', default=False)
+def run(disk, cloud):
     """ Backup your assets """
     print(green('\nHello from DAM-Sync!'))
     print(green('-' * 80))
     print()
+
+    """
+    Collect options
+    """
+
+    sync_disk = False
+    sync_cloud = False
+
+    # by default sync all (no flags)
+    if not disk and not cloud:
+        sync_disk = True
+        sync_cloud = True
+
+    # sync all if both flags (weird, but ok)
+    if disk and cloud:
+        sync_disk = True
+        sync_cloud = True
+
+    # just sync disk
+    if disk and not cloud:
+        sync_disk = True
+        sync_cloud = False
+
+    # or just sync cloud
+    if cloud and not disk:
+        sync_disk = False
+        sync_cloud = True
+
+    """
+    Do the sync
+    """
+
+    # sync disk
+    if sync_disk:
+        print('Syncing to disk')
+
+    # sync cloud
+    if sync_cloud:
+        print('Syncing to S3')
+
